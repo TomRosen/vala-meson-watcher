d111cd5737a6ebdd2faf3ad676d0e0e02c879f6e
Tom Rosen <55321560+TomRosen@users.noreply.github.com>
2022-11-22 19:48:27 +0100

    Converted to TypeScript

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
-#! /usr/bin/env node
+import * as fs from 'fs';
 
-const fs = require('fs');
+let fileStructure: Map<string, string[]> = new Map();
+let fileWatchers: Map<string, fs.FSWatcher> = new Map();
 
-let fileStructure = new Map();
-let fileWatchers = new Map();
+let dir: string = './src';
+let mesonFilePath: string = './meson.build';
+let includeExtensions: string[] = ['.vala'];
+let runOnStart: boolean = false;
+let checkSubDirs: boolean = true;
 
-let dir = './src';
-let mesonFilePath = './meson.build';
-let includeExtensions = ['.vala'];
-let runOnStart = false;
-let checkSubDirs = true;
-
-let arg;
-let argIndex = 2;
+let arg: string;
+let argIndex: number = 2;
 while ((arg = process.argv[argIndex])) {
 	switch (arg) {
 		case '-d':
@@ -26,16 +24,16 @@ while ((arg = process.argv[argIndex])) {
 		case '-i':
 		case '--include':
 			if (process.argv[argIndex + 1].startsWith('.')) {
-				includeExtensions.push(process.argv[++argIndex].split(','));
+				includeExtensions.push(...process.argv[++argIndex].split(','));
 			} else {
 				console.error('Include extensions must start with a dot');
-				return;
+				process.exit(1);
 			}
 			break;
 		case '--ros':
 			if (
-				process.argv[argIndex + 1]?.toLowerCase === 'true' ||
-				process.argv[argIndex + 1]?.toLowerCase === 'false'
+				process.argv[argIndex + 1]?.toLowerCase() === 'true' ||
+				process.argv[argIndex + 1]?.toLowerCase() === 'false'
 			) {
 				runOnStart = process.argv[++argIndex].toLowerCase() === 'true';
 			} else {
@@ -44,8 +42,8 @@ while ((arg = process.argv[argIndex])) {
 			break;
 		case '--subdir':
 			if (
-				process.argv[argIndex + 1]?.toLowerCase === 'true' ||
-				process.argv[argIndex + 1]?.toLowerCase === 'false'
+				process.argv[argIndex + 1]?.toLowerCase() === 'true' ||
+				process.argv[argIndex + 1]?.toLowerCase() === 'false'
 			) {
 				checkSubDirs = process.argv[++argIndex].toLowerCase() === 'true';
 			}
@@ -62,12 +60,12 @@ while ((arg = process.argv[argIndex])) {
 			console.log('--ros                   Run on start');
 			console.log('-v, --version           Print version');
 			console.log('-h, --help              Display this help message');
-			return;
+			process.exit(0);
 			break;
 		case '-v':
 		case '--version':
 			console.log('vala-meson-watcher v0.1.0');
-			return;
+			process.exit(0);
 			break;
 		default:
 			console.error('Unknown argument: ' + arg);
@@ -78,7 +76,7 @@ while ((arg = process.argv[argIndex])) {
 
 if (!fs.existsSync(mesonFilePath)) {
 	console.error('Meson build file not found');
-	return;
+	process.exit(1);
 }
 
 addWatcher(dir);
@@ -86,17 +84,20 @@ getAllFilesFromDir(dir || './src');
 if (runOnStart) updateMeson();
 
 //watch for changes in the directory
-function addWatcher(path) {
+function addWatcher(path: string) {
 	console.log('Watching: ' + path);
-	let watcher = fs.watch(path || './src', (eventType, filename) => {
-		console.log(`${eventType} event occurred on ${filename}`);
-		getAllFilesFromDir(path || './src');
-		updateMeson();
-	});
+	let watcher: fs.FSWatcher = fs.watch(
+		path || './src',
+		(eventType: string, filename: string) => {
+			console.log(`${eventType} event occurred on ${filename}`);
+			getAllFilesFromDir(path || './src');
+			updateMeson();
+		}
+	);
 	fileWatchers.set(path, watcher);
 }
 
-function stripPrefix(str) {
+function stripPrefix(str: string) {
 	if (str.startsWith('/')) {
 		return str.substring(1);
 	} else if (str.startsWith('./')) {
@@ -107,10 +108,11 @@ function stripPrefix(str) {
 }
 
 function fileStructureToString() {
-	let str = '';
+	let str: string = '';
 	for (let [key, value] of fileStructure) {
-		let formattedKey = "'" + stripPrefix(key).replace(/\//g, "' / '") + "'";
-		for (let i = 0; i < value.length; i++) {
+		let formattedKey: string =
+			"'" + stripPrefix(key).replace(/\//g, "' / '") + "'";
+		for (let i: number = 0; i < value.length; i++) {
 			str += formattedKey + ' / ' + `'${value[i]}'` + ',\n';
 		}
 	}
@@ -118,25 +120,25 @@ function fileStructureToString() {
 }
 
 function updateMeson() {
-	let mesonBuildFile = fs.readFileSync(mesonFilePath, 'utf8');
-	let anchorTop = mesonBuildFile.indexOf('#vmw-anchor-top');
-	let anchorBottom = mesonBuildFile.indexOf('#vmw-anchor-bottom');
+	let mesonBuildFile: string = fs.readFileSync(mesonFilePath, 'utf8');
+	let anchorTop: number = mesonBuildFile.indexOf('#vmw-anchor-top');
+	let anchorBottom: number = mesonBuildFile.indexOf('#vmw-anchor-bottom');
 
 	if (anchorTop === -1 || anchorBottom === -1) {
 		console.error('Meson build file does not contain anchor');
 		return;
 	}
 	//remove everything between the top & bottom anchor
-	let mesonBuildFileWithoutAnchor =
+	let mesonBuildFileWithoutAnchor: string =
 		mesonBuildFile.slice(0, anchorTop + '#vmw-anchor-top'.length) +
 		mesonBuildFile.slice(anchorBottom);
 	//add the new files between the top & bottom anchor
-	let fileString = fileStructureToString();
+	let fileString: string = fileStructureToString();
 
 	anchorTop = mesonBuildFileWithoutAnchor.indexOf('#vmw-anchor-top');
 	anchorBottom = mesonBuildFileWithoutAnchor.indexOf('#vmw-anchor-bottom');
 
-	let mesonBuildFileWithNewFiles =
+	let mesonBuildFileWithNewFiles: string =
 		mesonBuildFileWithoutAnchor.slice(0, anchorTop + '#vmw-anchor-top'.length) +
 		'\n' +
 		fileString +
@@ -145,26 +147,23 @@ function updateMeson() {
 	fs.writeFileSync('./meson.build', mesonBuildFileWithNewFiles);
 }
 
-function getAllFilesFromDir(path) {
+function getAllFilesFromDir(path: string) {
 	fileStructure.set(path, []);
-	const filesInDir = () => {
-		files = fs.readdirSync(path, { withFileTypes: true });
-		files.forEach((file) => {
-			if (
-				file.isDirectory() &&
-				checkSubDirs &&
-				!fileWatchers.has(path + '/' + file.name)
-			) {
-				addWatcher(path + '/' + file.name);
-				getAllFilesFromDir(path + '/' + file.name);
-			} else {
-				if (includeExtensions.includes('.' + file.name.split('.').pop())) {
-					fileStructure.get(path).push(file.name);
-				}
+	let files: fs.Dirent[] = fs.readdirSync(path, { withFileTypes: true });
+	files.forEach((file: fs.Dirent) => {
+		if (
+			file.isDirectory() &&
+			checkSubDirs &&
+			!fileWatchers.has(path + '/' + file.name)
+		) {
+			addWatcher(path + '/' + file.name);
+			getAllFilesFromDir(path + '/' + file.name);
+		} else {
+			if (includeExtensions.includes('.' + file.name.split('.').pop())) {
+				fileStructure.get(path).push(file.name);
 			}
-		});
-	};
-	filesInDir();
+		}
+	});
 }
 
 //cleanup watch on close
