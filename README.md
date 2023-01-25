91ed96ad1afcdb62a8261519c2357ae5404a5a54
TomRosen <msmasterg@gmail.com>
2023-01-25 18:34:17 +0100

    Added colored logging

--- README.md
+++ README.md
@@ -4,24 +4,25 @@ A tool that automatically looks for files in a directory and adds them as source
 
 <!--toc:start-->
 
-- [Installation](#installation)
-- [Build from source](#build-from-source)
-- [Usage](#usage)
-- [Options](#options)
-- [Requirements](#requirements)
-- [Examples](#examples)
-- [Note](#note)
-<!--toc:end-->
+- [vala-meson-watcher](#vala-meson-watcher)
+  - [Installation](#installation)
+  - [Build from source](#build-from-source)
+  - [Usage](#usage)
+  - [Options](#options)
+  - [Requirements](#requirements)
+  - [Examples](#examples)
+  - [Note](#note)
+  <!--toc:end-->
 
 ## Installation
 
-Run `npx vala-meson-watcher` to install the tool.
+Run `npx vala-meson-watcher` to run the tool.
 
 ## Build from source
 
 Run `npm run build` to build the TypeScript source code into JavaScript.\
 Make sure you have TypeScript installed globally (`npm i -g typescript`).\
-Run `npm install -g` to install the tool globally.
+Run `npm install -g` to install the tool globally on your system.
 
 ## Usage
 
@@ -55,7 +56,7 @@ This command will watch the directory `/path/to/src` for changes, and when it de
 vala-meson-watcher --ros
 ```
 
-This command will watch the default directory `./src` for changes, and when it detects changes it will add the new files to the default meson build file located at `./meson.build`. It will include files with all extensions by default.
+This command will watch the default directory `./src` for changes, and when it detects changes it will add the new files to the default meson build file located at `./meson.build`. It will include files with the `.vala` extensions by default.
 
 ## Note
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import * as fs from "fs";
 
+const enum LogType {
+  default = "",
+  red = "\x1b[31m%s\x1b[0m",
+  green = "\x1b[32m%s\x1b[0m",
+  yellow = "\x1b[33m%s\x1b[0m",
+  blue = "\x1b[34m%s\x1b[0m",
+}
+
 let fileStructure: Map<string, string[]> = new Map();
 let fileWatchers: Map<string, fs.FSWatcher> = new Map();
 
@@ -8,6 +16,7 @@ let mesonFilePath: string = "./meson.build";
 let includeExtensions: string[] = [".vala"];
 let runOnStart: boolean = false;
 let checkSubDirs: boolean = true;
+let logging: boolean = true;
 
 let arg: string;
 let argIndex: number = 2;
@@ -26,7 +35,7 @@ while ((arg = process.argv[argIndex])) {
       if (process.argv[argIndex + 1].startsWith(".")) {
         includeExtensions.push(...process.argv[++argIndex].split(","));
       } else {
-        console.error("Include extensions must start with a dot");
+        console.error(LogType.red, "Include extensions must start with a dot");
         process.exit(1);
       }
       break;
@@ -61,21 +70,23 @@ while ((arg = process.argv[argIndex])) {
       console.log("-v, --version           Print version");
       console.log("-h, --help              Display this help message");
       process.exit(0);
-      break;
     case "-v":
     case "--version":
       console.log("vala-meson-watcher v0.1.1");
       process.exit(0);
-      break;
     default:
-      console.error("Unknown argument: " + arg);
+      console.error(LogType.red, "Unknown argument: " + arg);
       break;
   }
   argIndex++;
 }
 
+function log(message: string, type: LogType = LogType.default): void {
+  if (logging) console.log(type, message);
+}
+
 if (!fs.existsSync(mesonFilePath)) {
-  console.error("Meson build file not found");
+  console.error(LogType.red, "Meson build file not found");
   process.exit(1);
 }
 
@@ -84,12 +95,12 @@ getAllFilesFromDir(dir || "./src");
 if (runOnStart) updateMeson();
 
 //watch for changes in the directory
-function addWatcher(path: string) {
-  console.log("Watching: " + path);
+function addWatcher(path: string): void {
+  log(`Watching ${path}`);
   let watcher: fs.FSWatcher = fs.watch(
     path || "./src",
     (eventType: string, filename: string) => {
-      console.log(`${eventType} event occurred on ${filename}`);
+      log(`${eventType} event occurred on ${filename}`);
       getAllFilesFromDir(path || "./src");
       updateMeson();
     }
@@ -97,7 +108,7 @@ function addWatcher(path: string) {
   fileWatchers.set(path, watcher);
 }
 
-function stripPrefix(str: string) {
+function stripPrefix(str: string): string {
   if (str.startsWith("/")) {
     return str.substring(1);
   } else if (str.startsWith("./")) {
@@ -107,11 +118,10 @@ function stripPrefix(str: string) {
   }
 }
 
-function fileStructureToString() {
+function fileStructureToString(): string {
   let str: string = "";
   for (let [key, value] of fileStructure) {
-    let formattedKey: string =
-      "'" + stripPrefix(key).replace(/\//g, "' / '") + "'";
+    let formattedKey: string = `'${stripPrefix(key).replace(/\//g, "' / '")}'`;
     for (let i: number = 0; i < value.length; i++) {
       str += formattedKey + " / " + `'${value[i]}'` + ",\n";
     }
@@ -119,13 +129,13 @@ function fileStructureToString() {
   return str;
 }
 
-function updateMeson() {
+function updateMeson(): void {
   let mesonBuildFile: string = fs.readFileSync(mesonFilePath, "utf8");
   let anchorTop: number = mesonBuildFile.indexOf("#vmw-anchor-top");
   let anchorBottom: number = mesonBuildFile.indexOf("#vmw-anchor-bottom");
 
   if (anchorTop === -1 || anchorBottom === -1) {
-    console.error("Meson build file does not contain anchor");
+    console.error(LogType.red, "Meson build file does not contain anchor");
     return;
   }
   //remove everything between the top & bottom anchor
@@ -138,28 +148,27 @@ function updateMeson() {
   anchorTop = mesonBuildFileWithoutAnchor.indexOf("#vmw-anchor-top");
   anchorBottom = mesonBuildFileWithoutAnchor.indexOf("#vmw-anchor-bottom");
 
-  let mesonBuildFileWithNewFiles: string =
-    mesonBuildFileWithoutAnchor.slice(0, anchorTop + "#vmw-anchor-top".length) +
-    "\n" +
-    fileString +
-    mesonBuildFileWithoutAnchor.slice(anchorBottom);
+  let mesonBuildFileWithNewFiles: string = `${mesonBuildFileWithoutAnchor.slice(
+    0,
+    anchorTop + "#vmw-anchor-top".length
+  )}\n${fileString}${mesonBuildFileWithoutAnchor.slice(anchorBottom)}`;
 
   fs.writeFileSync("./meson.build", mesonBuildFileWithNewFiles);
 }
 
-function getAllFilesFromDir(path: string) {
+function getAllFilesFromDir(path: string): void {
   fileStructure.set(path, []);
   let files: fs.Dirent[] = fs.readdirSync(path, { withFileTypes: true });
-  files.forEach((file: fs.Dirent) => {
+  files.forEach((file: fs.Dirent): void => {
     if (
       file.isDirectory() &&
       checkSubDirs &&
       !fileWatchers.has(path + "/" + file.name)
     ) {
-      addWatcher(path + "/" + file.name);
-      getAllFilesFromDir(path + "/" + file.name);
+      addWatcher(`${path}/${file.name}`);
+      getAllFilesFromDir(`${path}/${file.name}`);
     } else {
-      if (includeExtensions.includes("." + file.name.split(".").pop())) {
+      if (includeExtensions.includes(`.${file.name.split(".").pop()}`)) {
         fileStructure.get(path).push(file.name);
       }
     }
@@ -168,9 +177,9 @@ function getAllFilesFromDir(path: string) {
 
 //cleanup watch on close
 process.on("SIGINT", () => {
-  console.log("Closing...");
+  log("Closing...");
   fileWatchers.forEach((watcher, path) => {
-    console.log("Closing watcher: " + path);
+    log(`Closing watcher: ${path}`);
     watcher.close();
   });
   process.exit();
